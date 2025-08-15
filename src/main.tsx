64feebab76eb7d372a59125643aec13ddf41a730
Mckali-2k <kalid538598@gmail.com>
2025-08-15 14:36:41 +0300

    updated main.tsx
    conflict resolved

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,5 @@ import App from './App.tsx'
 import './index.css'
 import { I18nProvider } from '@/contexts/I18nContext'
 
-<<<<<<< Current (Your changes)
-createRoot(document.getElementById("root")!).render(<App />);
-=======
+
 createRoot(document.getElementById("root")!).render(<I18nProvider><App /></I18nProvider>);
->>>>>>> Incoming (Background Agent changes)
